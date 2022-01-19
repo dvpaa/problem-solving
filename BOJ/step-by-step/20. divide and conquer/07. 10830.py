aafcf75d16c6d2ed5b9a57c2c342b13dc4daaf71
Geonu Kim <82706622+dvpaa@users.noreply.github.com>
2022-01-19 03:36:11 +0000

    correct num 7

--- BOJ/step-by-step/20. divide and conquer/07. 10830.py
+++ BOJ/step-by-step/20. divide and conquer/07. 10830.py
@@ -1,6 +1,7 @@
 # 10830 행렬 제곱
 
-def met_mul(A: list, B: list, N: int) -> list:
+def met_mul(A: list, B: list) -> list:
+    N = len(A)
     C = [[0] * N for _ in range(N)]
     
     for i in range(N):
@@ -10,22 +11,24 @@ def met_mul(A: list, B: list, N: int) -> list:
             C[i][j] %= 1000
     return C
 
-def met_exp(A: list, B: int, N: int) -> list:
+def met_exp(A: list, B: int) -> list:
+    N = len(A)
     if B == 1:
+        for i in range(N):
+            for j in range(N):
+                A[i][j] %= 1000
         return A
-    elif B == 2:
-        return met_mul(A, A, N)
     else:
-        temp = met_exp(A, B//2, N)
+        temp = met_exp(A, B//2)
         if B % 2 == 0:
-            return met_mul(temp, temp, N)
+            return met_mul(temp, temp)
         else:
-            return met_mul(met_mul(temp, temp, N), A, N)
+            return met_mul(met_mul(temp, temp), A)
 
 N, B = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(N)]
 
-result = met_exp(A, B, N)
+result = met_exp(A, B)
 for i in range(N):
     for j in range(N):
         print(result[i][j], end = ' ')
