1b0613e33a84c7a389ef0c3d574ba2d5c13f21af
dvpaa <kgo0926@naver.com>
2022-09-10 17:19:37 +0900

    solution of BOJ_1351

---
+++ BOJ/1351. 무한 수열 /1351.py
@@ -0,0 +1,17 @@
+# 무한 수열
+
+import math
+
+N, P, Q = map(int, input().split())
+A = dict()
+A[0] = 1
+
+
+def arr(i: int):
+    if i in A:
+        return A[i]
+    A[i] = arr(i//P) + arr(i//Q)
+    return A[i]
+
+
+print(arr(N))
