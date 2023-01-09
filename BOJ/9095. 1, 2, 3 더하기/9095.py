af7e711e4e6a426b6bf468d450287c56fab0da30
dvpaa <kgo0926@naver.com>
2023-01-09 13:00:22 +0900

    solution of BOJ 9019

---
+++ BOJ/9095. 1, 2, 3 더하기/9095.py
@@ -0,0 +1,23 @@
+# 1, 2, 3 더하기
+
+import sys
+
+input = sys.stdin.readline
+
+dp = [0] * 12
+
+
+def dfs(i):
+    if i > 11:
+        return
+
+    dp[i] += 1
+    for j in range(1, 4):
+        dfs(i + j)
+
+
+dfs(0)
+
+for _ in range(int(input())):
+    n = int(input())
+    print(dp[n])
