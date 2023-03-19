037d8e4d8a7549ae13892999eb7edd3b219ae450
dvpaa <kgo000926@gmail.com>
2023-03-20 00:51:11 +0900

    BOJ 1406 python

---
+++ boj/1406. 에디터/1406.py
@@ -0,0 +1,28 @@
+import sys
+
+fast_input = sys.stdin.readline
+
+left_stack = list(fast_input().rstrip())
+right_stack = []
+
+for s in str:
+    left_stack.append(s)
+
+
+for _ in range(int(fast_input())):
+    operator = fast_input().rstrip().split()
+    if operator[0] == "L" and left_stack:
+        right_stack.append(left_stack.pop())
+
+    elif operator[0] == "D" and right_stack:
+        left_stack.append(right_stack.pop())
+
+    elif operator[0] == "B" and left_stack:
+        left_stack.pop()
+
+    elif operator[0] == "P":
+        left_stack.append(operator[1])
+
+
+print("".join(left_stack), end="")
+print("".join(right_stack[::-1]))
